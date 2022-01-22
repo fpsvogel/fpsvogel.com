187e9c4beeb1523345d1da2c63a8b31ab66b86fd
Felipe Vogel <fps.vogel@gmail.com>
2022-01-21 19:11:41 -0500

    Load Stimulus controller

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,10 +1,21 @@
 import "index.css"
+import { Application } from "@hotwired/stimulus"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
 console.info("Bridgetown is loaded!")
 
-const application = Application.start()
-const context = require.context("./controllers", true, /.js$/)
-application.load(definitionsFromContext(context))
+window.Stimulus = Application.start()
+
+import controllers from "./controllers/**/*.{js,js.rb}"
+Object.entries(controllers).forEach(([filename, controller]) => {
+  if (filename.includes("_controller.")) {
+    const identifier = filename.replace("./controllers/", "")
+      .replace(/_controller..*$/, "")
+      .replace("_", "-") // THIS IS THE LINE I ADDED
+      .replace("/", "--")
+
+    Stimulus.register(identifier, controller.default)
+  }
+})
