4f5bcc419be3e5b954523f2f96f4ccd096dc66da
Felipe Vogel <fps.vogel@gmail.com>
2022-01-21 21:48:40 -0500

    Add Turbo

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,5 +1,13 @@
 import "index.css"
 import { Application } from "@hotwired/stimulus"
+import * as Turbo from "@hotwired/turbo"
+
+// Uncomment the line below to add transition animations when Turbo navigates.
+// We recommend adding <meta name="turbo-cache-control" content="no-preview" />
+// to your HTML head if you turn on transitions. Use data-turbo-transition="false"
+// on your <main> element for pages where you don't want any transition animation.
+//
+// import "./turbo_transitions.js"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
