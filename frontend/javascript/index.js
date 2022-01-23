d98b9fc5b64a61c7d521d87e2d73ba7eb2bd4c45
Felipe Vogel <fps.vogel@gmail.com>
2022-01-23 15:57:01 -0500

    Fix index.js to properly register Stimulus controllers with multi-word names

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -18,10 +18,10 @@ window.Stimulus = Application.start()
 
 import controllers from "./controllers/**/*.{js,js.rb}"
 Object.entries(controllers).forEach(([filename, controller]) => {
-  if (filename.includes("_controller.")) {
+  if (filename.includes("_controller.") || filename.includes("-controller.")) {
     const identifier = filename.replace("./controllers/", "")
-      .replace(/_controller..*$/, "")
-      .replace("_", "-") // THIS IS THE LINE I ADDED
+      .replace(/[_\-]controller\..*$/, "")
+      .replace("_", "-")
       .replace("/", "--")
 
     Stimulus.register(identifier, controller.default)
