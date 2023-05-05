de142965c3195e074c5ab065b80538ae7a3aad4f
Felipe Vogel <fps.vogel@gmail.com>
2023-05-05 17:24:38 -0400

    Clean up (spacing, indentation, comments) reading list component

--- frontend/javascript/controllers/reading_list_controller.js
+++ frontend/javascript/controllers/reading_list_controller.js
@@ -1,34 +1,37 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "container", "filters", "table" ]
+  static targets = ["container", "filters", "table"]
 
-  // runs on page load.
+  // Runs on page load.
   connect() {
     this.resetOptions()
     this.expandAtAnchor()
   }
 
-  // resets sort and filters.
+  // Resets sort and filter options.
   resetOptions() {
     let firstSort = this.containerTarget.querySelector("rl-sorts input:first-of-type")
     if (firstSort) {
       firstSort.checked = true
     }
+
     let filters = this.filtersTarget.querySelectorAll("input")
     for (const filter of filters) {
       filter.checked = true
     }
+
     let starRatingFilter = this.filtersTarget.querySelector("#filter-rating-star")
     if (starRatingFilter) {
       starRatingFilter.checked = false
     }
   }
 
-  // expands the item at the URL's anchor (if any).
+  // Expands the item at the URL's anchor (if any).
   expandAtAnchor() {
     let anchor = window.location.hash.substr(1)
     let anchorItem = this.tableTarget.querySelector(`rl-item[item-id='${anchor}']`)
+
     if (anchorItem != null) {
       anchorItem.classList.add("expanded")
       anchorItem.classList.remove("collapsed")
@@ -36,17 +39,19 @@ export default class extends Controller {
     }
   }
 
-  // checks/unchecks all according to the "toggle all" checkbox (event.currentTarget).
+  // Checks/unchecks all according to the "toggle all" checkbox (event.currentTarget).
   toggleRatingCheckboxes(event) {
     let newState = event.currentTarget.checked
     let ratings = this.filtersTarget.querySelectorAll('[id^="filter-rating-"]')
+
     for (const rating of ratings) {
       rating.checked = newState
     }
+
     this.filter()
   }
 
-  // checks/unchecks all according to the "toggle all" checkbox (event.currentTarget).
+  // Checks/unchecks all according to the "toggle all" checkbox (event.currentTarget).
   toggleGenreCheckboxes(event) {
     let newState = event.currentTarget.checked
     let genres = this.filtersTarget.querySelectorAll('[id^="filter-genre-"]')
@@ -56,7 +61,7 @@ export default class extends Controller {
     this.filter()
   }
 
-  // expands the rl-filters element to show all filters, when "show all" is clicked (event.target).
+  // Expands the rl-filters element to show all filters, when "show all" is clicked (event.target).
   expandFilters(event) {
     event.target.display = "none"
     event.target.parentElement.textContent = "Filters: "
@@ -64,46 +69,52 @@ export default class extends Controller {
     this.filtersTarget.classList.remove("collapsed")
   }
 
-  // shows/hides items (rows) by genre and/or rating.
+  // Shows/hides items (rows) by genre and/or rating.
   filter() {
-    let selectedGenres = Array.from(this.filtersTarget.querySelectorAll('[id^="filter-genre-"]'))
-                              .map(genre => {
-                                if (genre.checked)
-                                  return genre.value
-                                else
-                                  return null
-                              })
-                              .filter(genre => genre)
-    let selectedRatings = Array.from(this.filtersTarget.querySelectorAll('[id^="filter-rating-"]'))
-                              .map(rating => {
-                                if (rating.checked)
-                                  return rating.value
-                                else
-                                  return null
-                              })
-                              .filter(rating => rating)
+    let selectedGenres = Array
+      .from(this.filtersTarget.querySelectorAll('[id^="filter-genre-"]'))
+      .map(genre => {
+        if (genre.checked)
+          return genre.value
+        else
+          return null
+      })
+      .filter(genre => genre)
+
+    let selectedRatings = Array
+      .from(this.filtersTarget.querySelectorAll('[id^="filter-rating-"]'))
+      .map(rating => {
+        if (rating.checked)
+          return rating.value
+        else
+          return null
+      })
+      .filter(rating => rating)
+
     this.tableTarget.querySelectorAll("rl-item").forEach(item => {
       let byRating = this.showByRating(item, selectedRatings)
       let byGenre = this.showByGenre(item, selectedGenres)
+
       if (byRating && byGenre) {
         if (item.parentElement.tagName == "HIDDEN-ITEM-WRAPPER") {
-          // show item.
+          // Show the item.
           item.style.display = "block"
           item.parentElement.outerHTML = item.outerHTML
         }
       } else if (item.parentElement.tagName != "HIDDEN-ITEM-WRAPPER") {
-        // hide item.
+        // Hide the item.
         item.style.display = "none"
         item.outerHTML = "<hidden-item-wrapper>" + item.outerHTML + "</hidden-item-wrapper>"
       }
     })
   }
 
-  // returns whether item passes the rating filter.
+  // Whether item passes the rating filter.
   showByRating(item, selectedRatings) {
     let itemRating = item.querySelector("rl-rating").textContent.trim()
     let starRatingFilter = this.filtersTarget.querySelector("#filter-rating-star")
-    if (starRatingFilter) { // star rating mode.
+
+    if (starRatingFilter) { // star rating mode
       if (!starRatingFilter.checked) {
         return true
       } else {
@@ -112,78 +123,92 @@ export default class extends Controller {
         }
         return false
       }
-    } else { // number rating mode (ratingFilter is a menu).
+    } else { // number rating mode (ratingFilter is a menu)
       return selectedRatings.includes(itemRating)
     }
   }
 
-  // returns whether item passes the genre filters.
+  // Whether item passes the genre filters.
   showByGenre(item, selectedGenres) {
     let itemGenres = Array.from(item.querySelectorAll("rl-genre")).map(genre => genre.textContent)
     let intersection = itemGenres.filter(g => selectedGenres.includes(g.trim()))
+
     if (intersection.length == 0) {
       return false
     }
     return true
   }
 
-  // sorts items (rows) by date or rating.
+  // Sorts items (rows) by date or rating.
   sort() {
     let selectedSort = this.containerTarget.querySelector('rl-sorts input:checked').value
+
     if (this.multipleValuesAreVisible(selectedSort)) {
-      let sorted = Array.from(this.tableTarget.children).sort((itemA, itemB) => {
-        let valueA = itemA.querySelector("rl-" + selectedSort).textContent.trim()
-        let valueB = itemB.querySelector("rl-" + selectedSort).textContent.trim()
-        if (valueA > valueB || valueA == "in progress")
-          return 1
-        else if (valueA < valueB)
-          return -1
-        else { // tie breaker: date (if rating sort is selected) or alphabetical order
-          let altSort = selectedSort === "rating" ? "date" : "name"
-          let altA = itemA.querySelector(`rl-${altSort}`).textContent.trim().toLowerCase()
-          let altB = itemB.querySelector(`rl-${altSort}`).textContent.trim().toLowerCase()
-          if (altA > altB)
+      let sorted = Array
+        .from(this.tableTarget.children)
+        .sort((itemA, itemB) => {
+          let valueA = itemA.querySelector("rl-" + selectedSort).textContent.trim()
+          let valueB = itemB.querySelector("rl-" + selectedSort).textContent.trim()
+
+          if (valueA > valueB || valueA == "in progress")
             return 1
-          else if (altA < altB)
+          else if (valueA < valueB)
             return -1
-          else
-            return 0
-        }
-      }).reverse()
+          else { // tie breaker: date (if rating sort is selected) or alphabetical order
+            let altSort = selectedSort === "rating" ? "date" : "name"
+            let altA = itemA.querySelector(`rl-${altSort}`).textContent.trim().toLowerCase()
+            let altB = itemB.querySelector(`rl-${altSort}`).textContent.trim().toLowerCase()
+
+            if (altA > altB)
+              return 1
+            else if (altA < altB)
+              return -1
+            else
+              return 0
+          }
+        })
+        .reverse()
+
       sorted.forEach(item => this.tableTarget.appendChild(item))
     }
   }
 
+  // Whether the currently visible (i.e. filtered) items have multiple values of the given
+  // type, e.g. rating or date.
   multipleValuesAreVisible(valueName) {
-    let visibleValues = Array.from(this.tableTarget.querySelectorAll("rl-" + valueName))
-                             .filter((el) => !this.isDescendant(el, "HIDDEN-ITEM-WRAPPER"))
-                             .map((el) => el.textContent.trim())
+    let visibleValues = Array
+      .from(this.tableTarget.querySelectorAll("rl-" + valueName))
+      .filter((el) => !this.isDescendant(el, "HIDDEN-ITEM-WRAPPER"))
+      .map((el) => el.textContent.trim())
+
     function unique(value, index, self) {
       return self.indexOf(value) === index;
     }
+
     return (visibleValues.filter(unique).length > 1)
   }
 
-  isDescendant(child, ancestorTagName) {
-    let node = child.parentNode
+  // Whether element is the descendent of an ancestorTagName.
+  isDescendant(element, ancestorTagName) {
+    let node = element.parentNode
+
     while (node != null) {
       if (node.tagName == ancestorTagName) {
         return true
       }
       node = node.parentNode
     }
+
     return false
   }
 
-  // expands/collapses an item (row).
+  // Expands/collapses an item (row).
   expandOrCollapse(event) {
     let clickedTag = event.target.tagName
     let item = event.currentTarget
-    // let collapsedBlurbClicked = item.classList.contains("collapsed") && clickedTag == "RL-BLURB"
     let nameClicked = clickedTag == "A"
     let notesClicked = clickedTag == "P"
-    // let blurbClicked = clickedTag == "RL-BLURB"
-    // if (collapsedBlurbClicked || (!nameClicked && !notesClicked && !blurbClicked)) {
+
     if (!nameClicked && !notesClicked) {
       if (item.classList.contains("collapsed")) {
         item.classList.add("expanded")
