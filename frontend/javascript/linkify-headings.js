b238893fdbe9189e98f18eb494f2fb4289db93f5
Felipe Vogel <fps.vogel@gmail.com>
2025-11-06 01:45:47 -0500

    Highlight heading after following a link to it

--- frontend/javascript/linkify-headings.js
+++ frontend/javascript/linkify-headings.js
@@ -1,13 +1,17 @@
-document.addEventListener("turbo:load", linkifyHeadings)
+document.addEventListener("turbo:load", linkifyHeadings);
 
 /* Adds anchor links to headings. */
 function linkifyHeadings(_event) {
-  const headings = document.querySelectorAll("h2[id], h3[id], h4[id], h5[id], h6[id]");
-
+  const headings = document.querySelectorAll(
+    "h2[id], h3[id], h4[id], h5[id], h6[id]"
+  );
   for (const heading of headings) {
     heading.innerHTML =
-      '<a href="#' + heading.id + '">' +
-          heading.innerText +
-      '</a>';
+      '<a href="#' + heading.id + '">' + heading.innerText + "</a>";
+  }
+
+  const internalLinks = document.querySelectorAll("a[href^='#'], a[href^='/']");
+  for (const link of internalLinks) {
+    link.setAttribute("data-turbo", "false");
   }
 }
